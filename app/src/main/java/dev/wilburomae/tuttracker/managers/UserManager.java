0a6649ac02a9c00429ca0c623cac399af6b568ae
Wilbur Omae <wilburomae@gmail.com>
2020-11-05 11:29:04 +0300

    Remove unused methods and fields

--- app/src/main/java/dev/wilburomae/tuttracker/managers/UserManager.java
+++ app/src/main/java/dev/wilburomae/tuttracker/managers/UserManager.java
@@ -7,12 +7,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import dev.wilburomae.tuttracker.models.User;
 
 public class UserManager {
-    private static DatabaseReference database = FirebaseDatabase.getInstance().getReference();
-    private static DatabaseReference dbUsers = database.child("users");
-
-    public static DatabaseReference FetchUser(String id) {
-        return dbUsers.child(id);
-    }
+    private static final DatabaseReference dbUsers = FirebaseDatabase.getInstance().getReference().child("users");
 
     public static void CreateUser(FirebaseUser firebaseUser) {
         User user = new User(firebaseUser.getUid(), firebaseUser.getDisplayName(), firebaseUser.getEmail());
